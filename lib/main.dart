b322d9ea70e7eec07db4d9ef5fefa41643d4f490
Arpit.Lkrwle <imarpitt@gmail.com>
2021-10-23 12:29:55 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:first_app/page/onboarding_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
+    );
+
+    return MaterialApp(
+      title: 'Introduction screen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: OnBoardingPage(),
+    );
+  }
+}
+
+
+
